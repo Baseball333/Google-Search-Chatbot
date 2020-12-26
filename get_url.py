dbcbcb59825264322abd628dbf18e8e278ec3662
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-26 14:54:11 -0800

    Variable assignations
    Write the page, tree and soup variables

--- get_url.py
+++ get_url.py
@@ -8,5 +8,8 @@ query = "How old is Jane Doe"
 # Query search results as a Python list of URLs
 search_result_list = list(search(query, tld="co.in", num=10, stop=3, pause=1))
 
+page = requests.get(search_result_list[index])
 
+tree = html.fromstring(page.content)
 
+soup = BeautifulSoup(page.content, features="lxml")
