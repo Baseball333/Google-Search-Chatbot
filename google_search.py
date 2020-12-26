e38d12ac941a792b9182c1a49b28368b216480db
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-26 14:12:16 -0800

    Query the results into a list

--- google_search.py
+++ google_search.py
@@ -1,3 +1,7 @@
 from googlesearch import search
 
 query = "How old is Jane Doe"
+
+# Query the results into a list of URLS
+search_result_list = list(search(query, tld="co.in", num=10, stop=3, pause=1))
+
