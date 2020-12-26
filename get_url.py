e87140f4603b635d81aa423c1941ff649e4be9b6
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-26 14:50:40 -0800

    Query the search results as a list of URLs

--- get_url.py
+++ get_url.py
@@ -2,3 +2,11 @@ import requests
 from  import html
 from googlesearch import search
 from bs4 import BeautifulSoup
+
+query = "How old is Jane Doe"
+
+# Query search results as a Python list of URLs
+search_result_list = list(search(query, tld="co.in", num=10, stop=3, pause=1))
+
+
+
