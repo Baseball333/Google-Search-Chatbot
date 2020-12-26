f358a7c67fb03b8414e98eb0bba271153fa8a42a
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-26 15:10:11 -0800

    Try construct

--- googlesearch.py
+++ googlesearch.py
@@ -5,3 +5,7 @@ from bs4 import BeautifulSoup
 
 # To search print the query method
 def chatbot_query(query, index=0):
+    fallback = "Sorry, I cannot think of a reply for that."
+    result = ""
+    
+    try:
